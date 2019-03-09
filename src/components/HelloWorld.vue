b356f765cacb6b2170bfb1b7f3e8d2924e8b33ff
zxy <you@example.com695033188@qq.com>
2019-03-09 17:45:50 +0800

    zxy

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <div>hello world</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ["message"]
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+</style>
