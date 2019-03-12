8853b576864adbbb8bd014c80e5c7414dfbdd252
zxy <you@example.com695033188@qq.com>
2019-03-12 14:21:22 +0800

    weekend

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <div class="recommend-title">热销推荐</div>
+    <div class="recommend-title">周末去哪</div>
     <ul>
       <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
-        <img class="item-img" :src="item.imgUrl" />
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
           <p class="item-desc">{{item.desc}}</p>
-          <button class="item-button">查看详情</button>
         </div>
       </li>
     </ul>
@@ -16,27 +17,27 @@
 
 <script>
 export default {
-  name: 'HomeRecommend',
+  name: 'HomeWeekend',
   data () {
     return {
       recommendList: [{
         id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1812/c1/c156e13ddd316b2ba3.img.jpg_200x200_4dcee529.jpg',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
         title: '北京活的3D博物馆',
         desc: '有八座古寺而得名，香山外另一爬山绝佳地有八座古寺而得名，香山外另一爬山绝佳地'
       }, {
         id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/201307/18/73703f0304835b9ac8d65eac.jpg_200x200_12107416.jpg',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/7e/476589267ebb41.jpg_r_640x214_bf599709.jpg',
         title: '北京活的3D博物馆',
         desc: '有八座古寺而得名，香山外另一爬山绝佳地有八座古寺而得名，香山外另一爬山绝佳地'
       }, {
         id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1505/f5/f5f45e1a83537bcb.water.jpg_200x200_444cc7e8.jpg',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
         title: '北京活的3D博物馆',
         desc: '有八座古寺而得名，香山外另一爬山绝佳地有八座古寺而得名，香山外另一爬山绝佳地'
       }, {
         id: '0004',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1703/88/88b6e81d8506213a3.img.jpg_200x200_6d009208.jpg',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg',
         title: '北京活的3D博物馆',
         desc: '有八座古寺而得名，香山外另一爬山绝佳地有八座古寺而得名，香山外另一爬山绝佳地'
       }]
@@ -52,33 +53,24 @@ export default {
     line-height: .44rem
     background: #eee
     text-indent: .2rem
-  .item
+  .item-img-wrapper
     overflow: hidden
-    display: flex
-    height: 1.9rem
-    .item-img
-      width: 1.7rem
-      height: 1.7rem
-      padding: .1rem
-    .item-info
-      flex: 1
-      padding: .1rem
-      min-width: 0
-      .item-title
-        line-height: .54rem
-        font-size: .32rem
-        ellipsis()
-      .item-desc
-        overflow: hidden
-        line-height: .4rem
-        height: .4rem
-        color: #ccc
-        ellipsis()
-      .item-button
-        line-height: .44rem
-        margin-top: .2rem
-        background: #ff9300
-        padding: 0 .2rem
-        color: #fff
-        border-radius: .06rem
+    height: 0
+    padding-bottom: 33.9%
+  .item-img
+    width: 100%
+  .item-info
+    flex: 1
+    padding: .1rem
+    min-width: 0
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      overflow: hidden
+      line-height: .4rem
+      height: .4rem
+      color: #ccc
+      ellipsis()
 </style>
