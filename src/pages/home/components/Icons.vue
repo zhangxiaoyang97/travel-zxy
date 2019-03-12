2afaa84d0ba830df2f9e222390d68736683c927e
zxy <you@example.com695033188@qq.com>
2019-03-12 13:59:33 +0800

    index-recommend

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -58,12 +58,10 @@ export default {
       const pages = []
       this.iconList.forEach((item, index) => {
         const page = Math.floor(index / 8)
-        console.log(page)
         if (!pages[page]) {
           pages[page] = []
         }
         pages[page].push(item)
-        console.log(pages)
       })
       return pages
     }
@@ -76,29 +74,31 @@ export default {
   .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-  .icon
-    position: relative
-    float: left
-    width: 25%
-    height: 0
-    padding-bottom: 25%
-    text-align: center
-    .icon-img
-      position: absolute
-      top: 0
-      left: 0
-      right: 0
-      bottom: .44rem
-      .icon-img-content
-        display: block
-        margin: 0 auto
-        height: 100%
-    .icon-desc
-      position: absolute
-      left: 0
-      right: 0
-      bottom: 0
-      height: .44rem
-      line-height: .44rem
-      ellipsis()
+  .icons
+    margin-top: .1rem
+    .icon
+      position: relative
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      text-align: center
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        ellipsis()
 </style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1812/c1/c156e13ddd316b2ba3.img.jpg_200x200_4dcee529.jpg',
+        title: '北京活的3D博物馆',
+        desc: '有八座古寺而得名，香山外另一爬山绝佳地有八座古寺而得名，香山外另一爬山绝佳地'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201307/18/73703f0304835b9ac8d65eac.jpg_200x200_12107416.jpg',
+        title: '北京活的3D博物馆',
+        desc: '有八座古寺而得名，香山外另一爬山绝佳地有八座古寺而得名，香山外另一爬山绝佳地'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1505/f5/f5f45e1a83537bcb.water.jpg_200x200_444cc7e8.jpg',
+        title: '北京活的3D博物馆',
+        desc: '有八座古寺而得名，香山外另一爬山绝佳地有八座古寺而得名，香山外另一爬山绝佳地'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1703/88/88b6e81d8506213a3.img.jpg_200x200_6d009208.jpg',
+        title: '北京活的3D博物馆',
+        desc: '有八座古寺而得名，香山外另一爬山绝佳地有八座古寺而得名，香山外另一爬山绝佳地'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/mixins.styl'
+  .recommend-title
+    margin-top: .2rem
+    line-height: .44rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        overflow: hidden
+        line-height: .4rem
+        height: .4rem
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .2rem
+        background: #ff9300
+        padding: 0 .2rem
+        color: #fff
+        border-radius: .06rem
+</style>
